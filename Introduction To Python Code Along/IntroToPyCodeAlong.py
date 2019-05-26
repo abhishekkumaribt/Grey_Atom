e0d90ba8f4b653ba51d9e3c294fbe3db1aaebdeb
Abhishek Kumar <abhishekkumaribt@gmail.com>
2019-05-26 14:08:07 +0530

    Introduction to python code along added

---
+++ Introduction To Python Code Along/IntroToPyCodeAlong.py
@@ -0,0 +1,28 @@
+import yaml
+
+path = "C:/Users/WONDER/Downloads/file.yaml"
+data = yaml.safe_load(open(path))
+
+typeOfData = type(data)
+
+city = data['info']['city']
+venue = data['info']['venue']
+
+teams = data['info']['teams']
+teamsCount = len(teams)
+
+tossWinner = data['info']['toss']['winner']
+winnerDecision = data['info']['toss']['decision']
+
+firstBaller = data['innings'][0]['1st innings']['deliveries'][0][0.1]['bowler']
+firstBatsman = data['innings'][0]['1st innings']['deliveries'][0][0.1]['batsman']
+
+totalDelevries1stInning = len(data['innings'][0]['1st innings']['deliveries'])
+totalDelevries2ndInning = len(data['innings'][1]['2nd innings']['deliveries'])
+
+winner = data['info']['outcome']['winner']
+winnerBy = data['info']['outcome']['by']
+
+print(typeOfData,city,venue,teams,teamsCount,tossWinner,winnerDecision,sep="\n")
+print(firstBaller,firstBatsman,totalDelevries1stInning,totalDelevries2ndInning,sep="\n")
+print(winner,winnerBy,sep="\n")
